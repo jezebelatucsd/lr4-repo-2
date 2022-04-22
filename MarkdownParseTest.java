ae046ca1a8474ff3bf08dc55f33f2908a1fd5f9a
aaronchan32 <chanaaron32@gmail.com>
2022-04-22 10:42:10 -0700

    Added Tests 2-4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -13,9 +13,33 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testGetLinks() throws IOException {
+    public void testGetLinksFile1() throws IOException {
         List<String> answer = List.of("https://something.com", "some-thing.html");
         Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        assertEquals(answer, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testGetLinksFile2() throws IOException {
+        List<String> answer = List.of();
+        Path fileName = Path.of("test-2.md");
+        String content = Files.readString(fileName);
+        assertEquals(answer, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testGetLinksFile3() throws IOException {
+        List<String> answer = List.of();
+        Path fileName = Path.of("test-3.md");
+        String content = Files.readString(fileName);
+        assertEquals(answer, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testGetLinksFile4() throws IOException {
+        List<String> answer = List.of("https://en.wikipedia.org/wiki/Day_One_(app)");
+        Path fileName = Path.of("test-4.md");
         String content = Files.readString(fileName);
         assertEquals(answer, MarkdownParse.getLinks(content));
     }
