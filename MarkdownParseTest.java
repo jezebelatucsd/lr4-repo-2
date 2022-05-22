41c5e2bb53f2763e52b2057a39e051471569550f
jezebelatucsd <103277481+jezebelatucsd@users.noreply.github.com>
2022-05-21 21:57:09 -0700

    added tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -125,5 +125,35 @@ public class MarkdownParseTest {
         assertEquals(answer, MarkdownParse.getLinks(content));
     }
     
+    @Test
+    public void snippet1Test() throws IOException {
+        Path fileName = Path.of("snippet1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("`google.com");
+        expected.add("google.com");
+        expected.add("ucsd.edu");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void snippet2Test() throws IOException {
+        Path fileName = Path.of("snippet2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("a.com");
+        expected.add("a.com(())");
+        expected.add("example.com");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void snippet3Test() throws IOException {
+        Path fileName = Path.of("snippet3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
 
 }
